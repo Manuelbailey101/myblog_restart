b46bf41c01841e0252ad546a207191f55019287e
manuel <manuelbailey722@gmail.com>
2023-02-28 20:09:01 -0800

    113 #4

--- posts/views.py
+++ posts/views.py
@@ -10,7 +10,6 @@ class PostListView(ListView):
     template_name = "post_list.html"
     model = Post
 
-
 class PostDetailView(DetailView):
     template_name = "post_detail.html"
     model = Post
